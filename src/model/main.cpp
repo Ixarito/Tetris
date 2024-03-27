4ce0430369c13ed28e9a173638a3f940869a9a84
60073 <60073@etu.he2b.be>
2024-03-27 10:18:09 +0100

    Tetris: Rename model

---
+++ src/model/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Projet DEV4 - 60453 & 60073 : Tetris" << endl;
+	cout << &b << endl << &c << endl << &d << endl;
+
+	return 0;
+}
