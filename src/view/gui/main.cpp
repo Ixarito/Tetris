290d3e7fb24bc3a0da8f5efc482d7be774005b04
60073 <60073@etu.he2b.be>
2024-05-15 17:06:24 +0200

    Fix: finally fix memory leaks

--- src/view/gui/main.cpp
+++ src/view/gui/main.cpp
@@ -7,21 +7,22 @@
 using namespace tetris;
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
+	QApplication a(argc, argv);
 
-	view::gui::MenuScene menuScene;
-	view::gui::TetrisScene tetrisScene;
+	auto menuScene = new view::gui::MenuScene();
+	auto tetrisScene = new view::gui::TetrisScene();
 
-	controller::gui::MenuController menuCtrl {menuScene};
-	controller::gui::GameController gameCtrl {tetrisScene};
+	auto menuCtrl = new controller::gui::MenuController(*menuScene);
+	auto gameCtrl = new controller::gui::GameController(*tetrisScene);
 
-	view::gui::MainWindow mainWindow {menuScene, tetrisScene};
+	view::gui::MainWindow mainWindow {*menuScene, *tetrisScene};
 
-	QObject::connect(&menuCtrl, &tetris::controller::gui::MenuController::requestGameStart, &gameCtrl, &tetris::controller::gui::GameController::newGame);
-	QObject::connect(&menuCtrl, &tetris::controller::gui::MenuController::requestDisplay, &mainWindow, &tetris::view::gui::MainWindow::changeScene);
-	QObject::connect(&gameCtrl, &tetris::controller::gui::GameController::requestDisplay, &mainWindow, &tetris::view::gui::MainWindow::changeScene);
-	QObject::connect(&tetrisScene, &tetris::view::gui::TetrisScene::gameOverConfirmed, [&](){
-		mainWindow.changeScene(&menuScene);
+	QObject::connect(menuCtrl, &tetris::controller::gui::MenuController::requestGameStart, gameCtrl, &tetris::controller::gui::GameController::newGame);
+	QObject::connect(menuCtrl, &tetris::controller::gui::MenuController::requestDisplay, &mainWindow, &tetris::view::gui::MainWindow::changeScene);
+	QObject::connect(gameCtrl, &tetris::controller::gui::GameController::requestDisplay, &mainWindow, &tetris::view::gui::MainWindow::changeScene);
+	QObject::connect(tetrisScene, &tetris::view::gui::TetrisScene::gameOverConfirmed, [&, menuScene](){
+		mainWindow.changeScene(menuScene);
 	});
-    return QApplication::exec();
+
+	return QApplication::exec();
 }
